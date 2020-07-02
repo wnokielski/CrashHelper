eb132e05b55c88395e7ec57356c83242e9835903
Wojtek <wojciech.nokielski@gmail.com>
2020-07-02 18:54:48 +0200

    Initial commit

---
+++ API/src/main/java/org/nokwoj/crashhelper/CrashhelperApplication.java
@@ -0,0 +1,13 @@
+package org.nokwoj.crashhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrashhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrashhelperApplication.class, args);
+    }
+
+}
---
+++ API/src/test/java/org/nokwoj/crashhelper/CrashhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nokwoj.crashhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
