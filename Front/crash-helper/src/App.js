6ceec1350e55d68967e7ed3fac2413ef5c35bd6e
Wojtek <wojciech.nokielski@gmail.com>
2020-10-13 21:55:42 +0200

    React router test

---
+++ Front/crash-helper/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import HomePage from './pages/HomePage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front/crash-helper/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePageStyle from '../styles/HomePage.css'
+
+class HomePage extends React.Component{
+    render(){
+        return (
+            <div>
+                Home page test
+            </div>   
+        );
+    }
+}
+
+export default HomePage;
