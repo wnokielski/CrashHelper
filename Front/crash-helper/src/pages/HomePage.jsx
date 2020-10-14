bd0ca8e735c42b03696215e161dc422afb749d1c
Wojtek <wojciech.nokielski@gmail.com>
2020-10-14 23:00:13 +0200

    Home page v2

--- Front/crash-helper/src/pages/HomePage.jsx
+++ Front/crash-helper/src/pages/HomePage.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import HomePageStyle from '../styles/HomePage.css'
-import Background from '../components/Background'
+import 'antd/dist/antd.css';
+import {Layout, Menu, Button} from 'antd'
+
+const { Header, Content, Footer } = Layout;
 
 class HomePage extends React.Component{
     render(){
         return (
-            <div className="home-background">
-                <Background />
-                <div className="let-us"></div>
-                <div className="ch"></div>
-            </div>   
+            <Layout>
+                <Header>
+                    <div className="crash-helper-logo"/>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">Home</Menu.Item>
+                        <Menu.Item key="2">About us</Menu.Item>
+                        <Menu.Item key="3" style={{ float: 'right' }}>Sign in</Menu.Item>
+                    </Menu>
+
+                </Header>
+                <Content>
+                    <div className="site-layout-content">
+                        <div className="let-us"></div>
+                        <div className="ch"></div>
+                    </div>
+                    <div className="button-div">
+                        <Button className="start-btn" type="primary" size="large">Start now</Button>
+                    </div>
+                </Content>
+                <Footer className="footer">Crash Helper ©2020 Created by W. Nokielski</Footer>
+            </Layout>
         );
     }
 }
