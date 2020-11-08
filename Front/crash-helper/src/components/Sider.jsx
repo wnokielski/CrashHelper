665b0531bc84376d80a0fc7e00153bd445680120
Wojtek <wojciech.nokielski@gmail.com>
2020-11-08 16:42:56 +0100

    Sider layout

--- Front/crash-helper/src/components/Sider.jsx
+++ Front/crash-helper/src/components/Sider.jsx
@@ -2,16 +2,6 @@ import React from "react";
 import "antd/dist/antd.css";
 import { Layout, Menu, Button } from "antd";
 import { Link } from "react-router-dom";
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -35,51 +25,19 @@ class MySider extends React.Component {
             <Menu
               mode="inline"
               theme="dark"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
+              //defaultSelectedKeys={["1"]}
+              //defaultOpenKeys={["sub1"]}
             >
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-              </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-              </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-              </Menu.Item>
-              <Menu.Item key="4" icon={<BarChartOutlined />}>
-                nav 4
-              </Menu.Item>
-              <Menu.Item key="5" icon={<CloudOutlined />}>
-                nav 5
-              </Menu.Item>
-              <Menu.Item key="6" icon={<AppstoreOutlined />}>
-                nav 6
-              </Menu.Item>
-              <Menu.Item key="7" icon={<TeamOutlined />}>
-                nav 7
-              </Menu.Item>
-              <Menu.Item key="8" icon={<ShopOutlined />}>
-                nav 8
-              </Menu.Item>
-              <Menu.Item key="9" icon={<UserOutlined />}>
-                nav 1
-              </Menu.Item>
-              <Menu.Item key="10" icon={<VideoCameraOutlined />}>
-                nav 2
-              </Menu.Item>
-              <Menu.Item key="11" icon={<UploadOutlined />}>
-                nav 3
-              </Menu.Item>
-              <Menu.Item key="12" icon={<BarChartOutlined />}>
-                nav 4
-              </Menu.Item>
-              <Menu.Item key="13" icon={<CloudOutlined />}>
-                nav 5
-              </Menu.Item>
-              <Menu.Item key="14" icon={<CloudOutlined />}>
-                nav 5
-              </Menu.Item>
+              <SubMenu key="sub1" title="Damages">
+                <Menu.Item key="1">New damages</Menu.Item>
+                <Menu.Item key="2">Priced damages</Menu.Item>
+                <Menu.Item key="3">Damages in progress</Menu.Item>
+                <Menu.Item key="4">Completed damages</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title="Opinions">
+                <Menu.Item key="5">My opinions</Menu.Item>
+                <Menu.Item key="6">Pending opinions</Menu.Item>
+              </SubMenu>
             </Menu>
           </Sider>
         </Layout>
@@ -101,24 +59,18 @@ class MySider extends React.Component {
             <Menu
               mode="inline"
               theme="dark"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
+              //defaultSelectedKeys={["1"]}
+              //defaultOpenKeys={["sub1"]}
             >
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-              </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-              </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-              </Menu.Item>
-              <Menu.Item key="4" icon={<BarChartOutlined />}>
-                nav 4
-              </Menu.Item>
-              <Menu.Item key="5" icon={<CloudOutlined />}>
-                nav 5
-              </Menu.Item>
+              <SubMenu key="sub1" title="Damages">
+                <Menu.Item key="1">Waiting for pricing</Menu.Item>
+                <Menu.Item key="2">Priced damages</Menu.Item>
+                <Menu.Item key="3">Damages in progress</Menu.Item>
+                <Menu.Item key="4">Completed damages</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title="Opinions">
+                <Menu.Item key="5">My opinions</Menu.Item>
+              </SubMenu>
             </Menu>
           </Sider>
         </Layout>
